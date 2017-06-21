29b21b7ee6f5949f60b2de8b4a5b2dedbe2e2a8c
HankChow <280630620@qq.com>
2017-06-21 16:12:34 +0800

    增加邮件通知模块

---
+++ sendmail.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+
+import smtplib
+import email.mime.multipart
+import email.mime.text
+
+
+def sendEmail(title, content, alsosendto=None): # alsosendto 为抄送列表
+
+    from_addr = '' # 发出邮箱
+    to_addr = [] # 目标邮箱
+    from_smtp_server = '' # 发出邮箱的 SMTP 服务器地址
+    from_smtp_port = '' # 发出邮箱的 SMTP 端口
+    from_password = '' # 发出邮箱的密码
+
+    msg = email.mime.multipart.MIMEMultipart()
+    msg['to'] = to_addr
+    msg['from'] = from_addr
+    msg['subject'] = title
+    content = content
+    
+    txt = email.mime.text.MIMEText(content)
+    msg.attach(txt)
+    
+    smtp = smtplib.SMTP()
+    smtp.connect(from_smtp_server, from_smtp_port) # 发出邮箱的 SMTP 服务器地址、端口
+    smtp.login(from_addr, from_password) # 发出邮箱的账号、密码
+    if alsosendto and len(alsosendto) > 0:
+        for emailAddr in alsosendto:
+            smtp.sendmail(from_addr, emailAddr, str(msg))
+    smtp.quit()
+
+
+if __name__ == '__main__':
+    sendEmail('title', 'content')
