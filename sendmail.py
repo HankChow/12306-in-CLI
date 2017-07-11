51dff13e3c67709f31ab41aff719ed55bb93d4a8
HankChow <280630620@qq.com>
2017-07-11 21:09:21 +0800

    更新sendmail模块

--- sendmail.py
+++ sendmail.py
@@ -6,32 +6,39 @@ import email.mime.multipart
 import email.mime.text
 
 
-def sendEmail(title, content, sendto=None): # sendto 为发送列表
+def sendEmail(title, content, sendto=[]): # sendto 为目标邮箱列表，支持多个
 
-    from_addr = '' # 发出邮箱
-    to_addr = [] # 目标邮箱
-    from_smtp_server = '' # 发出邮箱的 SMTP 服务器地址
-    from_smtp_port = '' # 发出邮箱的 SMTP 端口
-    from_password = '' # 发出邮箱的密码
+    for item in sendto:
+        msg = email.mime.multipart.MIMEMultipart()
+        msg['to'] = item 
+        msg['from'] = '' # 来源邮箱地址
+        msg['subject'] = title
+        content = content
+        smtp_server = '' # 来源邮箱的 SMTP 服务器地址
+        smtp_port = '' # 来源邮箱的 SMTP 端口
+        sender_password = '' # 来源邮箱的密码
 
-    msg = email.mime.multipart.MIMEMultipart()
-    msg['to'] = to_addr
-    msg['from'] = from_addr
-    msg['subject'] = title
-    content = content
-    
-    txt = email.mime.text.MIMEText(content)
-    msg.attach(txt)
-    
-    smtp = smtplib.SMTP()
-    smtp.connect(from_smtp_server, from_smtp_port) # 发出邮箱的 SMTP 服务器地址、端口
-    smtp.login(from_addr, from_password) # 发出邮箱的账号、密码
-    if sendto is None:
-        sendto = [to_addr]
-    if alsosendto and len(alsosendto) > 0:
-        for emailAddr in alsosendto:
-            smtp.sendmail(from_addr, emailAddr, str(msg))
-    smtp.quit()
+        if not msg['from']:
+            print('来源邮箱地址无效。')
+            exit()
+        if not smtp_server:
+            print('来源邮箱 SMTP 服务器地址无效。')
+            exit()
+        if not smtp_port:
+            print('来源邮箱 SMTP 端口无效。')
+            exit()
+        if not sender_password:
+            print('来源邮箱密码无效。')
+            exit()
+        
+        txt = email.mime.text.MIMEText(content)
+        msg.attach(txt)
+        
+        smtp = smtplib.SMTP()
+        smtp.connect(smtp_server, smtp_port)
+        smtp.login(msg['from'], sender_password)
+        smtp.sendmail(msg['from'], item, str(msg))
+        smtp.quit()
 
 
 if __name__ == '__main__':
